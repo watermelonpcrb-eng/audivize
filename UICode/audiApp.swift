989c42b3b016e06a85c819fcdb5a141ad2caf7a0
cleb <watermelonpcrb@gmail.com>
2026-01-31 20:11:02 -0800

    Refactor AppDelegate to use orientationLock property

--- UICode/audiApp.swift
+++ UICode/audiApp.swift
@@ -9,22 +9,25 @@
 import SwiftUI
 import UIKit
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    static var orientationManager = OrientationManager()
-    
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        return AppDelegate.orientationManager.orientationLock
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock: UIInterfaceOrientationMask = .all
+
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        AppDelegate.orientationLock
     }
 }
 
 @main
 struct audivApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(AppDelegate.orientationManager)
                 .ignoresSafeArea()
         }
     }
